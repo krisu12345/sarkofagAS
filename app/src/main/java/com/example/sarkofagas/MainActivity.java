63f5d788a7ac7a17aeb40dfa239a79a583c59804
Krzysztof Niedźwiedzki <K.Niedzwiedzki@student.zschie.edu.pl>
2022-01-05 00:15:19 +0100

    pokazują się kazde dane po przycisku

--- app/src/main/java/com/example/sarkofagas/MainActivity.java
+++ app/src/main/java/com/example/sarkofagas/MainActivity.java
@@ -32,16 +32,16 @@ public class MainActivity extends AppCompatActivity{
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_main);
 
-        String zmienna = "";
         Button button = findViewById(R.id.button);
         RadioButton radioButton = findViewById(R.id.radioButton);
+        RadioButton radioButton2 = findViewById(R.id.radioButton2);
+        RadioButton radioButton3 = findViewById(R.id.radioButton3);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(radioButton.isChecked()) {
-                    TextView tekst = (TextView) findViewById(R.id.textView2);
-                    tekst.setText("Sekretariat");
+
                     new Thread(new Runnable(){
 
                         @Override
@@ -51,9 +51,45 @@ public class MainActivity extends AppCompatActivity{
                             try {
                                 // Create a URL for the desired page
                                 URL url = new URL("https://raw.githubusercontent.com/krisu12345/Sarkofag/master/nauczyciel.txt");
-                                //URL urlp = new URL("https://raw.githubusercontent.com/krisu12345/Sarkofag/master/pracownik.txt");
-                                //URL urlu = new URL("https://raw.githubusercontent.com/krisu12345/Sarkofag/master/uczen.txt");//My text file location
-                                //First open the connection
+
+                                HttpURLConnection con=(HttpURLConnection)url.openConnection();
+                                con.setConnectTimeout(60000); // timing out in a minute
+
+                                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                                String str;
+                                while ((str = in.readLine()) != null) {
+                                    urls.add(str);
+                                }
+                                in.close();
+
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+
+                            MainActivity.this.runOnUiThread(new Runnable(){
+                                @Override
+                                public void run(){
+                                    TextView cos = (TextView) findViewById(R.id.dane);
+                                    cos.setText(urls.get(0));
+                                }
+
+                            });
+                        }
+                    }).start();
+                    }
+
+                if (radioButton2.isChecked()){
+
+                    new Thread(new Runnable(){
+
+                        @Override
+                        public void run() {
+                            ArrayList<String> urls=new ArrayList<String>();
+
+                            try {
+                                // Create a URL for the desired page
+                                URL url = new URL("https://raw.githubusercontent.com/krisu12345/Sarkofag/master/pracownik.txt");
 
                                 HttpURLConnection con=(HttpURLConnection)url.openConnection();
                                 con.setConnectTimeout(60000); // timing out in a minute
@@ -81,48 +117,46 @@ public class MainActivity extends AppCompatActivity{
                         }
                     }).start();
                 }
+
+                if (radioButton3.isChecked()){
+
+                    new Thread(new Runnable(){
+
+                        @Override
+                        public void run() {
+                            ArrayList<String> urls=new ArrayList<String>();
+
+                            try {
+                                // Create a URL for the desired page
+                                URL url = new URL("https://raw.githubusercontent.com/krisu12345/Sarkofag/master/uczen.txt");
+
+                                HttpURLConnection con=(HttpURLConnection)url.openConnection();
+                                con.setConnectTimeout(60000); // timing out in a minute
+
+                                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                                String str;
+                                while ((str = in.readLine()) != null) {
+                                    urls.add(str);
+                                }
+                                in.close();
+
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+
+                            MainActivity.this.runOnUiThread(new Runnable(){
+                                @Override
+                                public void run(){
+                                    TextView cos = (TextView) findViewById(R.id.dane);
+                                    cos.setText(urls.get(0));
+                                }
+
+                            });
+                        }
+                    }).start();
             }
+        };
         });
-
-        new Thread(new Runnable(){
-
-          @Override
-            public void run() {
-                ArrayList<String> urls=new ArrayList<String>();
-
-                try {
-                    // Create a URL for the desired page
-                    URL url = new URL("https://raw.githubusercontent.com/krisu12345/Sarkofag/master/pracownik.txt");
-                    //URL urlp = new URL("https://raw.githubusercontent.com/krisu12345/Sarkofag/master/pracownik.txt");
-                    //URL urlu = new URL("https://raw.githubusercontent.com/krisu12345/Sarkofag/master/uczen.txt");//My text file location
-                    //First open the connection
-
-                    HttpURLConnection con=(HttpURLConnection)url.openConnection();
-                    con.setConnectTimeout(60000); // timing out in a minute
-
-                    BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-
-                    String str;
-                    while ((str = in.readLine()) != null) {
-                        urls.add(str);
-                    }
-                    in.close();
-
-            } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-                MainActivity.this.runOnUiThread(new Runnable(){
-                    @Override
-                    public void run(){
-                        TextView cos = (TextView) findViewById(R.id.dane);
-                        cos.setText(urls.get(0));
-                    }
-
-                });
-            }
-        }).start();
-            };
-
-
+    }
 }
